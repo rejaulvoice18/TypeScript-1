fbccb25d642d55bb4af1d28fe67aeceaa7b1cbcb
rejaulvoice18 <rejaulvoice@gmail.com>
2025-07-06 15:32:24 +0100

    Added union and intersection

--- src/union-intersection.ts
+++ src/union-intersection.ts
@@ -1,10 +1,45 @@
+// Or hochhe Union
 
 type NoobDeveloper = {
     name: string;
 }
 
-type JuniorDeveloper = {
-    name:string;
-    expertise:string;
-    experience:number;
+// type JuniorDeveloper = {
+//     name:string;
+//     expertise:string;
+//     experience:number;
+// }
+
+// And means Intersection 2tai combine hoye ekta hoy
+
+type JuniorDeveloper = NoobDeveloper & {
+    expertise: string;
+    experience: number;
+}
+
+// Intersection er arekta expample
+
+// enum Level{
+//     junior = 'junior',
+//     mid = 'mid',
+//     senior = 'senior',
+// }
+
+type NextLevelDeveloper = JuniorDeveloper & {
+    leadershipExperience: number;
+    level: "junior" | "mid" | "senior";
+}
+
+const newDeveloper: NoobDeveloper | JuniorDeveloper = {
+    name: "Moznu Mia",
+    expertise: "JavaScript",
+    experience: 1,
+}
+
+const developer: NextLevelDeveloper ={
+    name: 'Md Rejaul Karim',
+    expertise: 'Typescript',
+    experience: 2,
+    leadershipExperience: 2,
+    level: "senior",
 }
